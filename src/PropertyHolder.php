bc64a54fb0b915512171f2a7fb7847d6f3cd3412
Daniel Kahl <kontakt@daniel-kahl.de>
2016-03-15 15:38:42 +0100

    Create PropertyHolder.php

---
+++ src/PropertyHolder.php
@@ -0,0 +1,33 @@
+<?php namespace Dtkahl\PropertyHolder;
+
+class PropertyHolder
+{
+
+  private $_properties;
+
+  public function __construct(array $properties = [])
+  {
+    $this->_properties = $properties;
+  }
+
+  public function has(string $key)
+  {
+
+  }
+
+  public function get(string $key, $default = null)
+  {
+
+  }
+
+  public function set(string $key, $value)
+  {
+
+  }
+
+  public function remove(string $key)
+  {
+
+  }
+
+}

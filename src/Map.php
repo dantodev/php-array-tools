e4b9d3b352875dc1eedaca7bd61a9b1c49b47702
Daniel Kahl <kontakt@daniel-kahl.de>
2016-03-24 10:45:29 +0100

    remove debug var_dump()

--- src/Map.php
+++ src/Map.php
@@ -76,7 +76,6 @@ class Map
    */
   public function merge($data)
   {
-    var_dump($data);
     if ($data instanceof self) {
       $this->_properties = array_merge($this->_properties, $data->all());
     } else {

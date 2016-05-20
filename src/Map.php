9425c5562becbbe68ac298b087546b768760366f
daniel <kontakt@daniel-kahl.de>
2016-05-20 15:55:55 +0200

    add PSR-2 compatibility #13

--- src/Map.php
+++ src/Map.php
@@ -2,249 +2,251 @@
 
 class Map
 {
-  /**
-   * @var array
-   */
-  private $_properties  = [];
-  private $_keys_locked = false;
+    /**
+     * @var array
+     */
+    private $_properties = [];
+    private $_keys_locked = false;
 
-  /**
-   * @param $key
-   * @return mixed
-   */
-  public function __get($key)
-  {
-    return $this->get($key);
-  }
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->get($key);
+    }
 
-  /**
-   * @param $key
-   * @param $value
-   */
-  public function __set($key, $value)
-  {
-    $this->set($key, $value);
-  }
+    /**
+     * @param $key
+     * @param $value
+     */
+    public function __set($key, $value)
+    {
+        $this->set($key, $value);
+    }
 
-  /**
-   * @param $key
-   * @return bool
-   */
-  public function __isset($key)
-  {
-    return $this->has($key);
-  }
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function __isset($key)
+    {
+        return $this->has($key);
+    }
 
-  /**
-   * @param $key
-   */
-  public function __unset($key)
-  {
-    $this->remove($key);
-  }
+    /**
+     * @param $key
+     */
+    public function __unset($key)
+    {
+        $this->remove($key);
+    }
 
-  /**
-   * Map constructor.
-   * @param array $properties
-   * @param bool $keys_locked
-   * @param bool $construct_recursive
-   */
-  public function __construct(array $properties = [], $keys_locked = false, $construct_recursive = false)
-  {
-    $this->_keys_locked = $keys_locked;
-    if ($construct_recursive) {
-      foreach ($properties as $key=>$value) {
-        if (is_array($value)) {
-          if (array_keys($value) === range(0, count($value) - 1)) {
-            $value = new Collection($value, false, true);
-          } else {
-            $value = new self($value, false, true);
-          }
+    /**
+     * Map constructor.
+     * @param array $properties
+     * @param bool $keys_locked
+     * @param bool $construct_recursive
+     */
+    public function __construct(array $properties = [], $keys_locked = false, $construct_recursive = false)
+    {
+        $this->_keys_locked = $keys_locked;
+        if ($construct_recursive) {
+            foreach ($properties as $key => $value) {
+                if (is_array($value)) {
+                    if (array_keys($value) === range(0, count($value) - 1)) {
+                        $value = new Collection($value, false, true);
+                    } else {
+                        $value = new self($value, false, true);
+                    }
+                }
+                $this->_properties[$key] = $value;
+            }
+        } else {
+            $this->_properties = $properties;
         }
-        $this->_properties[$key] = $value;
-      }
-    } else {
-      $this->_properties = $properties;
     }
-  }
 
-  /**
-   * @param $key
-   * @return bool
-   */
-  public function has($key)
-  {
-    return array_key_exists((string) $key, $this->_properties);
-  }
-
-  /**
-   * @param array $keys
-   * @return bool
-   */
-  public function hasKeys(array $keys)
-  {
-    return count(array_diff($keys, $this->getKeys())) === 0;
-  }
-
-  /**
-   * @param $key
-   * @param mixed $default
-   * @return null
-   */
-  public function get($key, $default = null)
-  {
-    return $this->has((string) $key) ? $this->_properties[(string) $key] : $default;
-  }
-
-  /**
-   * @param $key
-   * @param mixed $value
-   * @return $this
-   */
-  public function set($key, $value)
-  {
-    if (!$this->_keys_locked || $this->has($key)) {
-      $this->_properties[(string) $key] = $value;
-    } else {
-      throw new \RuntimeException("Unknown map key '$key'.'");
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        return array_key_exists((string)$key, $this->_properties);
     }
-    return $this;
-  }
 
-  /**
-   * @param $key
-   * @return $this
-   */
-  public function remove($key)
-  {
-    if ($this->has((string) $key)) {
-      if ($this->_keys_locked) {
-        $this->set($key, null);
-      } else {
-        unset($this->_properties[(string) $key]);
-      }
+    /**
+     * @param array $keys
+     * @return bool
+     */
+    public function hasKeys(array $keys)
+    {
+        return count(array_diff($keys, $this->getKeys())) === 0;
     }
-    return $this;
-  }
 
-  /**
-   * @param array $keys
-   * @return Map
-   */
-  public function except(array $keys)
-  {
-    return new self(array_diff_key($this->toArray(), array_flip($keys)));
-  }
-
-  /**
-   * @param array $keys
-   * @return Map
-   */
-  public function only(array $keys)
-  {
-    return new self(array_intersect_key($this->toArray(), array_flip($keys)));
-  }
-
-  /**
-   * @return array
-   */
-  public function toArray()
-  {
-    return $this->_properties;
-  }
-
-  /**
-   * @return array
-   */
-  public function toSerializedArray()
-  {
-    $array = [];
-    foreach ($this->toArray() as $key=>$item) {
-      $array[$key] = (is_object($item) && method_exists($item, 'toSerializedArray')) ?
-          $item->toSerializedArray() : json_decode(json_encode($item), true);  // Do anyone know a better way? :)
+    /**
+     * @param $key
+     * @param mixed $default
+     * @return null
+     */
+    public function get($key, $default = null)
+    {
+        return $this->has((string)$key) ? $this->_properties[(string)$key] : $default;
     }
-    return $array;
-  }
 
-  /**
-   * @return string
-   */
-  public function toJson()
-  {
-    return json_encode($this->toSerializedArray());
-  }
-
-  /**
-   * @return array
-   */
-  public function getKeys()
-  {
-    return array_keys($this->_properties);
-  }
-
-  /**
-   * @param $data
-   * @return $this
-   */
-  public function merge($data)
-  {
-    if ($data instanceof self) {
-      $this->_properties = array_merge($this->_properties, $data->toArray());
-    } else {
-      $this->_properties = array_merge($this->_properties, $data);
+    /**
+     * @param $key
+     * @param mixed $value
+     * @return $this
+     */
+    public function set($key, $value)
+    {
+        if (!$this->_keys_locked || $this->has($key)) {
+            $this->_properties[(string)$key] = $value;
+        } else {
+            throw new \RuntimeException("Unknown map key '$key'.'");
+        }
+        return $this;
     }
-    return $this;
-  }
 
-  /**
-   * @return Map
-   */
-  public function copy()
-  {
-    return clone $this;
-  }
-
-  /**
-   * @return $this
-   */
-  public function clear()
-  {
-    if ($this->_keys_locked) {
-      $this->_properties = array_map(function () {return null;}, $this->_properties);
-    } else {
-      $this->_properties = [];
+    /**
+     * @param $key
+     * @return $this
+     */
+    public function remove($key)
+    {
+        if ($this->has((string)$key)) {
+            if ($this->_keys_locked) {
+                $this->set($key, null);
+            } else {
+                unset($this->_properties[(string)$key]);
+            }
+        }
+        return $this;
     }
-    return $this;
-  }
 
-  /**
-   * @return int
-   */
-  public function count()
-  {
-    return count($this->_properties);
-  }
-
-  /**
-   * @return bool
-   */
-  public function isEmpty()
-  {
-  return $this->count() === 0;
-  }
-
-  /**
-   * @param \Closure $call
-   * @return $this
-   */
-  public function each(\Closure $call)
-  {
-    foreach ($this->_properties as $key=>$item) {
-      if ($call($key, $item) === false) {
-        break;
-      }
+    /**
+     * @param array $keys
+     * @return Map
+     */
+    public function except(array $keys)
+    {
+        return new self(array_diff_key($this->toArray(), array_flip($keys)));
     }
-    return $this;
-  }
+
+    /**
+     * @param array $keys
+     * @return Map
+     */
+    public function only(array $keys)
+    {
+        return new self(array_intersect_key($this->toArray(), array_flip($keys)));
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->_properties;
+    }
+
+    /**
+     * @return array
+     */
+    public function toSerializedArray()
+    {
+        $array = [];
+        foreach ($this->toArray() as $key => $item) {
+            $array[$key] = (is_object($item) && method_exists($item, 'toSerializedArray')) ?
+                $item->toSerializedArray() : json_decode(json_encode($item), true);  // Do anyone know a better way? :)
+        }
+        return $array;
+    }
+
+    /**
+     * @return string
+     */
+    public function toJson()
+    {
+        return json_encode($this->toSerializedArray());
+    }
+
+    /**
+     * @return array
+     */
+    public function getKeys()
+    {
+        return array_keys($this->_properties);
+    }
+
+    /**
+     * @param $data
+     * @return $this
+     */
+    public function merge($data)
+    {
+        if ($data instanceof self) {
+            $this->_properties = array_merge($this->_properties, $data->toArray());
+        } else {
+            $this->_properties = array_merge($this->_properties, $data);
+        }
+        return $this;
+    }
+
+    /**
+     * @return Map
+     */
+    public function copy()
+    {
+        return clone $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function clear()
+    {
+        if ($this->_keys_locked) {
+            $this->_properties = array_map(function () {
+                return null;
+            }, $this->_properties);
+        } else {
+            $this->_properties = [];
+        }
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->_properties);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEmpty()
+    {
+        return $this->count() === 0;
+    }
+
+    /**
+     * @param \Closure $call
+     * @return $this
+     */
+    public function each(\Closure $call)
+    {
+        foreach ($this->_properties as $key => $item) {
+            if ($call($key, $item) === false) {
+                break;
+            }
+        }
+        return $this;
+    }
 
 }
--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -1,217 +1,217 @@
 <?php namespace Dtkahl\ArrayTools;
 
-class CollectionTest extends \PHPUnit_Framework_TestCase {
+class CollectionTest extends \PHPUnit_Framework_TestCase
+{
 
-  /**
-   * @var Collection
-   */
-  public $collection;
-
-  private $_0 = ['first_name' => 'john', 'last_name' => 'smith', 'age' => 44];
-  private $_1 = ['first_name' => 'kara', 'last_name' => 'trace', 'age' => 27];
-  private $_2 = ['first_name' => 'phil', 'last_name' => 'mcKay', 'age' => 34];
-  private $_3 = ['first_name' => 'rose', 'last_name' => 'smith', 'age' => 27];
-
-
-  public function setUp()
-  {
-    $this->collection = new Collection([$this->_0, $this->_1, $this->_2, $this->_3]);
-  }
-
-  public function testToArray()
-  {
-    $this->assertCount(4, $this->collection->toArray());
-  }
-
-  public function testCount()
-  {
-    $this->assertEquals(4, $this->collection->count());
-  }
-
-  public function testToJson()
-  {
-    $this->assertEquals(
-        '[{"first_name":"john","last_name":"smith","age":44},{"first_name":"kara","last_name":"trace","age":27},{"first_name":"phil","last_name":"mcKay","age":34},{"first_name":"rose","last_name":"smith","age":27}]',
-        $this->collection->toJson()
-    );
-  }
-
-  public function testCopy()
-  {
-    $copy = $this->collection->copy();
-    $copy->push('test');
-    $this->assertEquals(4, $this->collection->count());
-    $this->assertEquals(5, $copy->count());
-  }
-
-  public function testHasKeyRemoveClearIndexes()
-  {
-    $this->assertEquals(true, $this->collection->hasKey(3));
-    $this->collection->remove(3);
-    $this->assertEquals(false, $this->collection->hasKey(3));
-  }
-
-  public function testIsEmptyClear()
-  {
-    $this->assertEquals(false, $this->collection->isEmpty());
-    $this->collection->clear();
-    $this->assertEquals(true, $this->collection->isEmpty());
-  }
-
-  public function testFirst()
-  {
-    $el = $this->collection->first();
-    $this->assertEquals($this->_0, $el);
-  }
-
-  public function testLast()
-  {
-    $el = $this->collection->last();
-    $this->assertEquals($this->_3, $el);
-  }
-
-  public function testGet()
-  {
-    $el = $this->collection->get(2);
-    $this->assertEquals($this->_2, $el);
-  }
-
-  public function testEach()
-  {
-    $arr = [];
-    $this->collection->each(function ($item, $key) use (&$arr)
-    {
-      $arr[$key] = $item;
-    });
-    $this->assertEquals([$this->_0, $this->_1, $this->_2, $this->_3], $arr);
-  }
-
-  public function testFilter()
-  {
-    $this->collection->filter(function ($item) {
-      return $item['last_name'] == 'smith';
-    });
-    $this->assertEquals(2, $this->collection->count());
-    $this->assertContains($this->_0, $this->collection->toArray());
-    $this->assertNotContains($this->_1, $this->collection->toArray());
-    $this->assertNotContains($this->_2, $this->collection->toArray());
-    $this->assertContains($this->_3, $this->collection->toArray());
-  }
-
-  public function testReverse()
-  {
-    $this->collection->reverse();
-    $this->assertEquals($this->_3, $this->collection->get(0));
-    $this->assertEquals($this->_2, $this->collection->get(1));
-    $this->assertEquals($this->_1, $this->collection->get(2));
-    $this->assertEquals($this->_0, $this->collection->get(3));
-  }
-
-  public function testPopPushPutShiftUnshiftInject()
-  {
-    // pop
-    $_3 = $this->collection->pop();
-    $this->assertEquals($this->_3, $_3);
-    $this->assertEquals($this->_2, $this->collection->last());
-    $this->assertEquals(3, $this->collection->count());
-
-    // push
-    $this->collection->push($_3);
-    $this->assertEquals($this->_3, $this->collection->last());
-
-    // put
-    $this->collection->put(2, 'test');
-    $this->assertEquals('test', $this->collection->get(2));
-
-    // shift
-    $_0 = $this->collection->shift();
-    $this->assertEquals($this->_0, $_0);
-    $this->assertEquals($this->_1, $this->collection->first());
-    $this->assertEquals(3, $this->collection->count());
-
-    // unshift
-    $this->collection->unshift($_0);
-    $this->assertEquals($this->_0, $this->collection->first());
-
-    // inject
-    $this->collection->inject(2, 'test2');
-    $this->assertEquals('test2', $this->collection->get(2));
-    $this->assertEquals(5, $this->collection->count());
-  }
-
-  public function testMergeArray()
-  {
-    $this->collection->merge(['test']);
-    $this->assertEquals(5, $this->collection->count());
-    $this->assertEquals('test', $this->collection->last());
-  }
-
-  public function testMergeCollection()
-  {
-    $this->collection->merge(new Collection(['test']));
-    $this->assertEquals(5, $this->collection->count());
-    $this->assertEquals('test', $this->collection->last());
-  }
-
-  public function testSort()
-  {
-    $this->collection->sort(function ($a, $b) {
-      if (preg_match('#^7.#', phpversion())) {
-        return $a['age'] > $b['age'];
-      } else {
-        return $a['age'] >= $b['age'];
-      }
-    });
-    $this->assertEquals([$this->_1, $this->_3, $this->_2, $this->_0], $this->collection->toArray());
-  }
-
-  public function testMap()
-  {
-    $this->collection->map(function ($item) {
-      return sprintf('%s %s', $item['first_name'], $item['last_name']);
-    });
-    $this->assertEquals(['john smith', 'kara trace', 'phil mcKay', 'rose smith'], $this->collection->toArray());
-  }
-
-  public function testChunk()
-  {
     /**
-     * @var Collection[] $chunks
+     * @var Collection
      */
-    $chunks = $this->collection->chunk(2);
-    $this->assertCount(2, $chunks);
-    $this->assertEquals($this->_0, $chunks[0]->get(0));
-    $this->assertEquals($this->_1, $chunks[0]->get(1));
-    $this->assertEquals($this->_2, $chunks[1]->get(0));
-    $this->assertEquals($this->_3, $chunks[1]->get(1));
-  }
+    public $collection;
 
-  public function testPointerIteration()
-  {
-    $this->assertEquals($this->_0, $this->collection->next());
-    $this->assertEquals($this->_1, $this->collection->next());
-    $this->assertEquals($this->_2, $this->collection->next());
-    $this->assertEquals($this->_3, $this->collection->next());
-    $this->assertEquals(null, $this->collection->next());
-    $this->assertEquals($this->_3, $this->collection->previous());
-    $this->assertEquals($this->_2, $this->collection->previous());
-    $this->assertEquals($this->_1, $this->collection->previous());
-    $this->assertEquals($this->_0, $this->collection->previous());
-    $this->assertEquals(null, $this->collection->previous());
-    $this->collection->setPointer(2);
-    $this->assertEquals($this->_2, $this->collection->current());
+    private $_0 = ['first_name' => 'john', 'last_name' => 'smith', 'age' => 44];
+    private $_1 = ['first_name' => 'kara', 'last_name' => 'trace', 'age' => 27];
+    private $_2 = ['first_name' => 'phil', 'last_name' => 'mcKay', 'age' => 34];
+    private $_3 = ['first_name' => 'rose', 'last_name' => 'smith', 'age' => 27];
 
-  }
 
-  public function testLists()
-  {
-    $list = $this->collection->lists(['first_name', 'last_name']);
-    $this->assertEquals([['first_name' => 'john', 'last_name' => 'smith'], ['first_name' => 'kara', 'last_name' => 'trace'], ['first_name' => 'phil', 'last_name' => 'mcKay'], ['first_name' => 'rose', 'last_name' => 'smith']], $list);
-  }
+    public function setUp()
+    {
+        $this->collection = new Collection([$this->_0, $this->_1, $this->_2, $this->_3]);
+    }
 
-  public function testJoin()
-  {
-    $this->assertEquals("a,b,c", (new Collection(["a", "b", "c"]))->join(","));
-  }
+    public function testToArray()
+    {
+        $this->assertCount(4, $this->collection->toArray());
+    }
+
+    public function testCount()
+    {
+        $this->assertEquals(4, $this->collection->count());
+    }
+
+    public function testToJson()
+    {
+        $this->assertEquals(
+            '[{"first_name":"john","last_name":"smith","age":44},{"first_name":"kara","last_name":"trace","age":27},{"first_name":"phil","last_name":"mcKay","age":34},{"first_name":"rose","last_name":"smith","age":27}]',
+            $this->collection->toJson()
+        );
+    }
+
+    public function testCopy()
+    {
+        $copy = $this->collection->copy();
+        $copy->push('test');
+        $this->assertEquals(4, $this->collection->count());
+        $this->assertEquals(5, $copy->count());
+    }
+
+    public function testHasKeyRemoveClearIndexes()
+    {
+        $this->assertEquals(true, $this->collection->hasKey(3));
+        $this->collection->remove(3);
+        $this->assertEquals(false, $this->collection->hasKey(3));
+    }
+
+    public function testIsEmptyClear()
+    {
+        $this->assertEquals(false, $this->collection->isEmpty());
+        $this->collection->clear();
+        $this->assertEquals(true, $this->collection->isEmpty());
+    }
+
+    public function testFirst()
+    {
+        $el = $this->collection->first();
+        $this->assertEquals($this->_0, $el);
+    }
+
+    public function testLast()
+    {
+        $el = $this->collection->last();
+        $this->assertEquals($this->_3, $el);
+    }
+
+    public function testGet()
+    {
+        $el = $this->collection->get(2);
+        $this->assertEquals($this->_2, $el);
+    }
+
+    public function testEach()
+    {
+        $arr = [];
+        $this->collection->each(function ($item, $key) use (&$arr) {
+            $arr[$key] = $item;
+        });
+        $this->assertEquals([$this->_0, $this->_1, $this->_2, $this->_3], $arr);
+    }
+
+    public function testFilter()
+    {
+        $this->collection->filter(function ($item) {
+            return $item['last_name'] == 'smith';
+        });
+        $this->assertEquals(2, $this->collection->count());
+        $this->assertContains($this->_0, $this->collection->toArray());
+        $this->assertNotContains($this->_1, $this->collection->toArray());
+        $this->assertNotContains($this->_2, $this->collection->toArray());
+        $this->assertContains($this->_3, $this->collection->toArray());
+    }
+
+    public function testReverse()
+    {
+        $this->collection->reverse();
+        $this->assertEquals($this->_3, $this->collection->get(0));
+        $this->assertEquals($this->_2, $this->collection->get(1));
+        $this->assertEquals($this->_1, $this->collection->get(2));
+        $this->assertEquals($this->_0, $this->collection->get(3));
+    }
+
+    public function testPopPushPutShiftUnshiftInject()
+    {
+        // pop
+        $_3 = $this->collection->pop();
+        $this->assertEquals($this->_3, $_3);
+        $this->assertEquals($this->_2, $this->collection->last());
+        $this->assertEquals(3, $this->collection->count());
+
+        // push
+        $this->collection->push($_3);
+        $this->assertEquals($this->_3, $this->collection->last());
+
+        // put
+        $this->collection->put(2, 'test');
+        $this->assertEquals('test', $this->collection->get(2));
+
+        // shift
+        $_0 = $this->collection->shift();
+        $this->assertEquals($this->_0, $_0);
+        $this->assertEquals($this->_1, $this->collection->first());
+        $this->assertEquals(3, $this->collection->count());
+
+        // unshift
+        $this->collection->unshift($_0);
+        $this->assertEquals($this->_0, $this->collection->first());
+
+        // inject
+        $this->collection->inject(2, 'test2');
+        $this->assertEquals('test2', $this->collection->get(2));
+        $this->assertEquals(5, $this->collection->count());
+    }
+
+    public function testMergeArray()
+    {
+        $this->collection->merge(['test']);
+        $this->assertEquals(5, $this->collection->count());
+        $this->assertEquals('test', $this->collection->last());
+    }
+
+    public function testMergeCollection()
+    {
+        $this->collection->merge(new Collection(['test']));
+        $this->assertEquals(5, $this->collection->count());
+        $this->assertEquals('test', $this->collection->last());
+    }
+
+    public function testSort()
+    {
+        $this->collection->sort(function ($a, $b) {
+            if (preg_match('#^7.#', phpversion())) {
+                return $a['age'] > $b['age'];
+            } else {
+                return $a['age'] >= $b['age'];
+            }
+        });
+        $this->assertEquals([$this->_1, $this->_3, $this->_2, $this->_0], $this->collection->toArray());
+    }
+
+    public function testMap()
+    {
+        $this->collection->map(function ($item) {
+            return sprintf('%s %s', $item['first_name'], $item['last_name']);
+        });
+        $this->assertEquals(['john smith', 'kara trace', 'phil mcKay', 'rose smith'], $this->collection->toArray());
+    }
+
+    public function testChunk()
+    {
+        /**
+         * @var Collection[] $chunks
+         */
+        $chunks = $this->collection->chunk(2);
+        $this->assertCount(2, $chunks);
+        $this->assertEquals($this->_0, $chunks[0]->get(0));
+        $this->assertEquals($this->_1, $chunks[0]->get(1));
+        $this->assertEquals($this->_2, $chunks[1]->get(0));
+        $this->assertEquals($this->_3, $chunks[1]->get(1));
+    }
+
+    public function testPointerIteration()
+    {
+        $this->assertEquals($this->_0, $this->collection->next());
+        $this->assertEquals($this->_1, $this->collection->next());
+        $this->assertEquals($this->_2, $this->collection->next());
+        $this->assertEquals($this->_3, $this->collection->next());
+        $this->assertEquals(null, $this->collection->next());
+        $this->assertEquals($this->_3, $this->collection->previous());
+        $this->assertEquals($this->_2, $this->collection->previous());
+        $this->assertEquals($this->_1, $this->collection->previous());
+        $this->assertEquals($this->_0, $this->collection->previous());
+        $this->assertEquals(null, $this->collection->previous());
+        $this->collection->setPointer(2);
+        $this->assertEquals($this->_2, $this->collection->current());
+
+    }
+
+    public function testLists()
+    {
+        $list = $this->collection->lists(['first_name', 'last_name']);
+        $this->assertEquals([['first_name' => 'john', 'last_name' => 'smith'], ['first_name' => 'kara', 'last_name' => 'trace'], ['first_name' => 'phil', 'last_name' => 'mcKay'], ['first_name' => 'rose', 'last_name' => 'smith']], $list);
+    }
+
+    public function testJoin()
+    {
+        $this->assertEquals("a,b,c", (new Collection(["a", "b", "c"]))->join(","));
+    }
 
 }
